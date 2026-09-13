e1cfbaeedca7b61d5aeae4e36829a6fb5d7cda1e
twirlyowes <harshpatil200808@gmail.com>
2026-09-13 15:15:02 +0530

    Delete xieron-helpdesk-bot-full-fixed directory

--- xieron-helpdesk-bot-full-fixed/src/utils/permissions.js
+++
@@ -1,8 +0,0 @@
-const {PermissionFlagsBits}=require('discord.js');
-const {ownerId,defaultModRoleId}=require('../config');
-function owner(i){return i.user.id===ownerId;}
-function admin(i){return owner(i)||i.memberPermissions?.has(PermissionFlagsBits.Administrator);}
-function mod(i){return admin(i)||!!defaultModRoleId&&i.member?.roles?.cache?.has(defaultModRoleId);}
-function targetOK(i,target){return target&&target.id!==i.user.id&&target.id!==i.client.user.id&&(!target.roles?.highest||!i.member?.roles?.highest||target.roles.highest.position<i.member.roles.highest.position);}
-function requireMod(i){return mod(i)?null:'You need moderator permissions for this command.';}
-module.exports={owner,admin,mod,targetOK,requireMod};
